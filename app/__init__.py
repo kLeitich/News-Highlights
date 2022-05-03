97f1fe69bee84f45aa0389799c2de40aad52211c
kleitich <kevleitich@gmail.com>
2022-05-03 21:40:31 +0300

    adding a new __init file to the repo

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,16 @@
 from flask import Flask
+from .config import DevConfig
 
-from app.config import DevConfig
 
-app = Flask(__name__)
+# Initializing application
+app = Flask(__name__,instance_relative_config= True)
+
+# Setting up configuration
 app.config.from_object(DevConfig)
+app.config.from_pyfile('config.py')
+
+
+# Initializing Flask Extensions
+# bootstrap = Bootstrap(app)
 
 from app import views
