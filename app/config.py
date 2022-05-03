55cb6ffd98595e5a22bc5eebd524676d84114f91
kleitich <kevleitich@gmail.com>
2022-05-03 14:43:17 +0300

    checking if the server is working getting no found error?

---
+++ app/config.py
@@ -0,0 +1,3 @@
+from flask import Flask
+from .config import Devconfig
+
---
+++ app/views.py
@@ -0,0 +1,9 @@
+from flask import render_template
+from app import app
+
+app.route('/')
+def index():
+    '''
+    view root page function
+    '''
+    return render_template('index.html')
