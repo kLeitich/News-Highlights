0faf28b79beb00b7c8a3d499038dbbbf1a7b1790
kleitich <kevleitich@gmail.com>
2022-05-03 21:41:19 +0300

    adding a new config file to repo

--- app/config.py
+++ app/config.py
@@ -1,9 +1,30 @@
-from flask import Flask
-from .config import DevConfig
+from tkinter import N
+
 
 class Config:
-    pass
+    '''
+    General configuration parent class
+    '''
+    NEWS_API_BASE_URL='https://newsapi.org/v2/top-headlines/sources?apiKey={}'
+
+
+
 class ProdConfig(Config):
+    '''
+    Production  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
     pass
+
+
 class DevConfig(Config):
+    '''
+    Development  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+
     DEBUG = True
